993df5af53fcdebd1f090f1d4e7e29f4f0a8d5cb
Dev Gill <26435947+sdevgill@users.noreply.github.com>
2023-05-22 09:01:21 +0100

    add try-except block and main function

--- length.py
+++ length.py
@@ -15,15 +15,15 @@ def get_media_file_duration(file_path):
         "format=duration",
         "-of",
         "default=noprint_wrappers=1:nokey=1",
-        file_path,
+        str(file_path),
     ]
-    result = subprocess.run(command, capture_output=True, text=True)
-    if result.returncode == 0:
-        try:
-            return float(result.stdout)
-        except ValueError:
-            pass
-    return 0
+    try:
+        result = subprocess.run(command, capture_output=True, text=True)
+        result.check_returncode()
+        return float(result.stdout)
+    except (subprocess.CalledProcessError, ValueError):
+        print(f"An error occurred while processing {file_path}.")
+        return 0
 
 
 def get_total_duration(directory):
@@ -35,7 +35,7 @@ def get_total_duration(directory):
         for f in directory.glob("**/*")
         if f.suffix.lower() in (".mp3", ".mp4", ".m4a", ".wav")
     )
-    total_duration = sum(get_media_file_duration(str(file)) for file in media_files)
+    total_duration = sum(get_media_file_duration(file) for file in media_files)
     return total_duration
 
 
@@ -43,7 +43,7 @@ def validate_directory_path(directory_path):
     """
     Validates the directory path provided as a command-line argument.
     """
-    if not Path(directory_path).is_dir():
+    if not directory_path.is_dir():
         print("Invalid directory path.")
         raise SystemExit(1)
 
@@ -57,25 +57,29 @@ def validate_command_line_arguments():
         raise SystemExit(1)
 
 
-# Validate the command-line arguments
-validate_command_line_arguments()
+def main():
+    # Validate the command-line arguments
+    validate_command_line_arguments()
 
-# Get the directory path from the command-line arguments
-directory_path = sys.argv[1]
+    # Get the directory path from the command-line arguments
+    directory_path = Path(sys.argv[1])
 
-# Validate the directory path
-validate_directory_path(directory_path)
+    # Validate the directory path
+    validate_directory_path(directory_path)
 
-# Get the total duration of all media files in the directory
-directory = Path(directory_path)
-total_duration = get_total_duration(directory)
+    # Get the total duration of all media files in the directory
+    total_duration = get_total_duration(directory_path)
 
-# Convert the total duration to hours, minutes, and seconds
-hours = int(total_duration / 3600)
-minutes = int((total_duration % 3600) / 60)
-seconds = int(total_duration % 60)
+    # Convert the total duration to hours, minutes, and seconds
+    hours = int(total_duration / 3600)
+    minutes = int((total_duration % 3600) / 60)
+    seconds = int(total_duration % 60)
 
-# Print the total duration of all media files in the directory
-print(
-    f"The total duration of media files in the directory is: {hours:02d}h:{minutes:02d}m:{seconds:02d}s."
-)
+    # Print the total duration of all media files in the directory
+    print(
+        f"The total duration of media files in the directory is: {hours:02d}h:{minutes:02d}m:{seconds:02d}s."
+    )
+
+
+if __name__ == "__main__":
+    main()
